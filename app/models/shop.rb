d82ac084cee4d568a3bd4c2ef566b8b75618ca29
Shoabe Shariff <shoabe.shariff@gmail.com>
2018-06-05 23:49:56 -0700

    Fix duplicate field specification in new shop form validation errors

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -11,11 +11,11 @@ class Shop < ApplicationRecord
   VALID_SUBDOMAIN_REGEX = /(^[A-Za-z0-9\-]*$)/
 
   def name_must_not_contain_only_whitespace
-    errors.add(:name, 'Name must not begin or end with whitespace') if INVALID_WHITESPACE_REGEX.match(name)
+    errors.add(:name, 'must not begin or end with whitespace') if INVALID_WHITESPACE_REGEX.match(name)
   end
 
   def subdomain_must_contain_only_valid_characters
-    errors.add(:subdomain, 'Subdomain may only contain alphanumeric characters') if !VALID_SUBDOMAIN_REGEX.match(subdomain)
+    errors.add(:subdomain, 'may only contain alphanumeric characters') if !VALID_SUBDOMAIN_REGEX.match(subdomain)
   end
 
   def urn(domain)
