b27496e4854b22cf2ceb987d08fc9875c3fea9e5
Shoabe Shariff <shoabe.shariff@gmail.com>
2018-06-06 22:16:51 -0700

    Validates email format on merchant sign up

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,7 +1,15 @@
 class Merchant < ApplicationRecord
-  has_secure_password
 
   has_many :shops, foreign_key: :merchant_fk
 
   validates_uniqueness_of :email
+  validate :email_must_be_valid_format
+
+  has_secure_password
+
+  VALID_EMAIL_REGEX = /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/
+
+  def email_must_be_valid_format
+    errors.add(:email, 'must be valid') if !VALID_EMAIL_REGEX.match(email)
+  end
 end
