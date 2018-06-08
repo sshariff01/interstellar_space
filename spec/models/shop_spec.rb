6505c6877d66f618a255e5183ddff3b5b4549e9a
Shoabe Shariff <shoabe.shariff@gmail.com>
2018-06-07 20:58:15 -0700

    WIP - Fix some Shop model specs

--- spec/models/shop_spec.rb
+++ spec/models/shop_spec.rb
@@ -5,11 +5,11 @@ describe Shop do
     describe ':name' do
       it 'creates an error if the name length is not between 3 and 40 characters' do
         name_too_short = Shop.create(:name => 'Hi')
-        name_too_long = Shop.create(:name => 'This is my Shop Name, and it is way too long')
+        name_too_long = Shop.create(:name => 'This is my Shop Name, and it is way too long' * 10)
         name_valid = Shop.create(:name => 'Valid Shop Name')
 
         expect(error_message_for(name_too_short, :name)).to include('is too short (minimum is 3 characters)')
-        expect(error_message_for(name_too_long, :name)).to include('is too long (maximum is 40 characters)')
+        expect(error_message_for(name_too_long, :name)).to include('is too long (maximum is 200 characters)')
         expect(error_message_for(name_valid, :name)).to be_empty
       end
 
@@ -19,9 +19,9 @@ describe Shop do
         name_whitespace_only = Shop.create(:name => '     ')
         name_valid = Shop.create(:name => 'Hello World')
 
-        expect(error_message_for(name_whitespace_start, :name)).to include('Name must not begin or end with whitespace')
-        expect(error_message_for(name_whitespace_end, :name)).to include('Name must not begin or end with whitespace')
-        expect(error_message_for(name_whitespace_only, :name)).to include('Name must not begin or end with whitespace')
+        expect(error_message_for(name_whitespace_start, :name)).to include('must not begin or end with whitespace')
+        expect(error_message_for(name_whitespace_end, :name)).to include('must not begin or end with whitespace')
+        expect(error_message_for(name_whitespace_only, :name)).to include('must not begin or end with whitespace')
         expect(error_message_for(name_valid, :name)).to be_empty
       end
     end
@@ -29,11 +29,11 @@ describe Shop do
     describe ':subdomain' do
       it 'creates an error if the subdomain length is not between 3 and 25 characters' do
         subdomain_too_short = Shop.create(:subdomain => 'hi')
-        subdomain_too_long = Shop.create(:subdomain => 'this-is-my-subdomain-name-which-is-way-too-long')
+        subdomain_too_long = Shop.create(:subdomain => 'this-is-my-subdomain-name-which-is-way-too-long' * 2)
         subdomain_valid = Shop.create(:subdomain => 'helloworld')
 
         expect(error_message_for(subdomain_too_short, :subdomain)).to include('is too short (minimum is 3 characters)')
-        expect(error_message_for(subdomain_too_long, :subdomain)).to include('is too long (maximum is 25 characters)')
+        expect(error_message_for(subdomain_too_long, :subdomain)).to include('is too long (maximum is 50 characters)')
         expect(error_message_for(subdomain_valid, :subdomain)).to be_empty
       end
 
@@ -43,9 +43,9 @@ describe Shop do
         subdomain_with_punctuation = Shop.create(:subdomain => 'inval1d,subd0ma19')
         subdomain_valid = Shop.create(:subdomain => 'val1d-subd0ma19')
 
-        expect(error_message_for(subdomain_with_underscore, :subdomain)).to include('Subdomain may only contain alphanumeric characters')
-        expect(error_message_for(subdomain_with_space, :subdomain)).to include('Subdomain may only contain alphanumeric characters')
-        expect(error_message_for(subdomain_with_punctuation, :subdomain)).to include('Subdomain may only contain alphanumeric characters')
+        expect(error_message_for(subdomain_with_underscore, :subdomain)).to include('may only contain alphanumeric characters')
+        expect(error_message_for(subdomain_with_space, :subdomain)).to include('may only contain alphanumeric characters')
+        expect(error_message_for(subdomain_with_punctuation, :subdomain)).to include('may only contain alphanumeric characters')
         expect(error_message_for(subdomain_valid, :subdomain)).to be_empty
       end
 
