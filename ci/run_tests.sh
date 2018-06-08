7af2806f5ad8d2ca130c83a7c6d145ec281ecfd1
Shoabe Shariff <shoabe.shariff@gmail.com>
2018-06-08 12:58:44 -0700

    fix - forgot 'yes'

--- ci/run_tests.sh
+++ ci/run_tests.sh
@@ -9,8 +9,8 @@ yes | apt-get install libnss3
 yes | apt-get install apt-transport-https libxss1 libappindicator1 libindicator7
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 sudo sh -c 'echo "deb https://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
-sudo apt-get update
-sudo apt-get install google-chrome-stable
+yes | sudo apt-get update
+yes | sudo apt-get install google-chrome-stable
 
 # yes | apt-get install libnss3
 # yes | apt-get install libxss1 libappindicator1 libindicator7
