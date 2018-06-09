7ab4e7b69585e8c43fe7932693e6de33f09c0324
Shoabe Shariff <shoabe.shariff@gmail.com>
2018-06-09 01:38:16 -0700

    Use postgres in CI
    - Includes cleaning up CI scripts by moving most of the set-up
    logic into the sshariff/rails-postgres Dockerfile
    
    [#158174904]

--- ci/run_acceptance_tests.sh
+++ ci/run_acceptance_tests.sh
@@ -1,17 +1,11 @@
 #!/usr/bin/env sh
 
-set -x
+set -ex
 
-apt-get update
-yes | apt-get install vim wget libsqlite3-dev nodejs
-
-yes | apt-get install libnss3
-yes | apt-get install apt-transport-https libxss1 libappindicator1 libindicator7
-wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
-sudo sh -c 'echo "deb https://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
-yes | sudo apt-get update
-yes | sudo apt-get install google-chrome-stable
+service postgresql start
 
 bundle install
+
 rake db:migrate
+
 rake test:acceptance
--- db/migrate/20180606032604_add_references_shop_to_merchant.rb
+++ db/migrate/20180606032604_add_references_shop_to_merchant.rb
@@ -1,6 +1,6 @@
 class AddReferencesShopToMerchant < ActiveRecord::Migration[5.1]
   def change
     add_column :shops, :merchant_fk, :integer, index: true
-    add_foreign_key :shops, :merchants, column: :merchant_fk
+    add_foreign_key :shops, :merchants, primary_key: :pk, column: :merchant_fk
   end
 end
