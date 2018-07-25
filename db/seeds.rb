6c0e84a230dbb4cd1e5094dc0c63d28ea9825036
Shoabe Shariff <shoabe.shariff@gmail.com>
2018-07-24 21:35:19 -0700

    Populates database with seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,35 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Product.destroy_all
+Shop.destroy_all
+Merchant.destroy_all
+
+merchant_1 = Merchant.create!({
+    name: 'Bundaberg',
+    email: 'merchant@bundaberg.com',
+    password: 'Bundaberg1',
+})
+
+shop_1 = Shop.create!({
+    merchant_fk: merchant_1.pk,
+    name: 'Bundaberg Bottled Drinks',
+    subdomain: 'bunda-bottles',
+})
+
+Product.create!({
+    shop_fk: shop_1.pk,
+    name: 'Ginger Beer',
+    price: 7.99,
+})
+
+Product.create!({
+    shop_fk: shop_1.pk,
+    name: 'Root Beer',
+    price: 5.99,
+})
+
+p "Created #{Merchant.count} merchant(s)!"
+p "Created #{Shop.count} shop(s)!"
+p "Created #{Product.count} product(s)!"
